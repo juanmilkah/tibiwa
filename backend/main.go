abf13d97d4d03154645d24fa3cc025a901da8195
Juan Milkah <juanmilkah@gmail.com>
2024-11-07 07:31:35 +0300

    format types

--- backend/main.go
+++ backend/main.go
@@ -1,9 +1,23 @@
 package main
 
 import (
-  "fmt"
+	"fmt"
+	"log"
+	"net/http"
 )
 
-func main(){
-  fmt.Println("Tibiwa backend")
+func RootHandler(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte("Connected Successfully"))
+}
+
+func main() {
+	http.HandleFunc("/api/", RootHandler)
+
+	fmt.Printf("Server listening on port 8000\n")
+	err := http.ListenAndServe(":8000", nil)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
 }
--- client/src/types/Types.ts
+++ client/src/types/Types.ts
@@ -1,41 +1,39 @@
 export type Appointment = {
-  id: string; // unique identifier for the appointment
-  patientId: string; // reference to the patient
-  healthcareProviderId: string; // reference to the healthcare provider
-  date: string; // date and time of the appointment (ISO 8601 format)
-  _type: "in-person" | "telehealth" | "home-visit"; // type of appointment
+  id: string;                    // unique identifier for the appointment
+  patientId : string;            // reference to the patient
+  healthcareProviderId : string; // reference to the healthcare provider
+  date : string; // date and time of the appointment (ISO 8601 format)
+  _type : "in-person" | "telehealth" | "home-visit"; // type of appointment
   location?: string; // optional for in-person appointments
-  purpose: string; // reason for the appointment
-  status: "scheduled" | "canceled" | "rescheduled"; // appointment status
-  completed: boolean; // true if the appointment has been completed
-  notes?: string; // optional notes from patient or provider
-  reminders?: boolean; // true if reminders are enabled
+  purpose : string;  // reason for the appointment
+  status : "scheduled" | "canceled" | "rescheduled"; // appointment status
+  completed : boolean;      // true if the appointment has been completed
+  notes?: string;           // optional notes from patient or provider
+  reminders?: boolean;      // true if reminders are enabled
   followUpNeeded?: boolean; // true if a follow-up is required
 };
 
 export type Medication = {
-  id: string; // Unique identifier for the medication
-  name: string; // Name of the medication
-  dosage: string; // Dosage amount (e.g., "500mg")
-  frequency: string; // Frequency of intake (e.g., "twice a day")
-  prescribedBy: string; // Healthcare provider who prescribed the medication
-  startDate: string; // Start date of the medication (ISO string format)
+  id: string;            // Unique identifier for the medication
+  name : string;         // Name of the medication
+  dosage : string;       // Dosage amount (e.g., "500mg")
+  frequency : string;    // Frequency of intake (e.g., "twice a day")
+  prescribedBy : string; // Healthcare provider who prescribed the medication
+  startDate : string;    // Start date of the medication (ISO string format)
   endDate?: string; // Optional end date for the medication (ISO string format)
-  instructions?: string; // Additional instructions for taking the medication
+  instructions?: string;  // Additional instructions for taking the medication
   sideEffects?: string[]; // Optional list of possible side effects
-  refillsLeft?: number; // Optional number of refills left
-  isActive: boolean; // Whether the medication is currently active
-  remindersEnabled?: boolean; // Whether reminders for taking the medication are enabled
+  refillsLeft?: number;   // Optional number of refills left
+  isActive : boolean;     // Whether the medication is currently active
+  remindersEnabled?:
+      boolean; // Whether reminders for taking the medication are enabled
 };
 
 export type Blog = {
-  id: number;
-  title: string;
-  authorId: string;
-  author: string;
-  preview: string;
-  content: string;
-  readTime: string;
-  category: string;
-  date: string;
+  id: number; title : string; authorId : string; author : string;
+  preview : string;
+  content : string;
+  readTime : string;
+  category : string;
+  date : string;
 }
