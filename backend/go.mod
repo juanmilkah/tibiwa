2d5f24b85aff1ded4899b1853f89e940f76188f5
Juan Milkah <juanmilkah@gmail.com>
2024-11-07 15:28:30 +0300

    add password user type

--- backend/go.mod
+++ backend/go.mod
@@ -2,4 +2,7 @@ module tibiwa
 
 go 1.23.2
 
-require github.com/mattn/go-sqlite3 v1.14.24
+require (
+	github.com/jmoiron/sqlx v1.4.0
+	github.com/mattn/go-sqlite3 v1.14.24
+)
--- backend/models/Users.go
+++ backend/models/Users.go
@@ -1,10 +1,21 @@
 package models
 
+import (
+	"database/sql"
+	"log"
+
+	"github.com/jmoiron/sqlx"
+)
+
 type User struct {
 	Id          string
 	Name        string
 	Email       string
 	AccountType string
+}
+
+type UserRepository struct {
+	db *sqlx.DB
 }
 
 func NewUser(data User) *User {
@@ -14,4 +25,20 @@ func NewUser(data User) *User {
 		Email:       data.Email,
 		AccountType: data.AccountType,
 	}
+}
+
+func NewUserRepostory(db *sqlx.DB) *UserRepository {
+	return &UserRepository{db: db}
+}
+
+func (r *UserRepository) GetById(id string) (*User, error) {
+	var user User
+	err := r.db.Get(&user, "select name, email, phonenumber, isProvider from users where id = $1", id)
+	if err == sql.ErrNoRows {
+		log.Fatal("No user found")
+		return nil, err
+	}
+
+	return &user, nil
+
 }
